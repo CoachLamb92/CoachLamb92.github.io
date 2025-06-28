16f90562ea1090f3b6c3eb8d5388a43d8e1cbfd9
Morgan Lamb <morgan.lamb@hotmail.com>
2025-06-28 14:54:29 +0100

    Edited a slight issue

--- puzzle_page.js
+++ puzzle_page.js
@@ -9,6 +9,7 @@ function delete_page() {
     document.getElementById("first_button").style.display = "none";
     document.getElementById("hidden_button").style.display = "none";
     document.getElementById("button_grid").style.display = "none";
+    document.getElementById("instructions").style.display = "none";
     setInterval('autoRefresh()', 2000);
 }
 
